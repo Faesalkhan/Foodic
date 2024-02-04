8472a613e765504b3d2ae4eb6612e505c729d428
Faesalkhan <pathanf9545@gmail.com>
2024-02-04 19:02:13 +0530

    updated shimmer according to buttons

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,6 @@
 const Shimmer = () => {
   return (
-    <div className="container bodycontainer">
+    <div className="container shimmercontainer">
       <div className="row justify-content-center">
         <div className="col-xs-8 col-sm-6 col-md-4 m-2 element-displaying  filterbutton"></div>
         <div className="col-1 m-2 element-displaying rounded-5 filterbutton "></div>
